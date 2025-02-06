357069f08ccbf8b009c2d8f7c13746b4d2fdc2ac
ryannevins <nevinsr@hartwick.edu>
2025-02-06 09:00:07 -0500

    Lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,3 +1,4 @@
-print("Hello World!")
-print("How's it going")
-print("I'm doing great")
+print("Lacrosse is the oldest game in the Americas")
+print("The game of lacrosse is often referred to as the fastest game on two feet")
+print("The game is extremely popular among Native Americans in Upstate New York and Canada")
+print("It is often referred to as the Creators Game by these groups: \"however the modern name of lacrosse is french")
